02331bdf75c10648094426f3a266d9f4ebb50675
DESKTOP-RBNO6LS\SSAFY <chera1009@naver.com>
2025-07-25 12:40:52 +0900

    chore: 프로젝트 초기 êµ구조 설정

---
+++ back/src/main/java/com/B108/tripwish/TripwishApplication.java
@@ -0,0 +1,14 @@
+package com.B108.tripwish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripwishApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TripwishApplication.class, args);
+	}
+
+}
