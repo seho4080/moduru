2c21e1f4adf9141e98f71be13db4223b3d087434
seho <tkrhktlqdjr1@naver.com>
2025-07-30 14:07:08 +0900

    [BUILD] docker-compose-clear

--- back/src/main/java/com/B108/tripwish/domain/user/dto/SignUpRequestDto.java
+++ back/src/main/java/com/B108/tripwish/domain/user/dto/SignUpRequestDto.java
@@ -1,22 +1,21 @@
 package com.B108.tripwish.domain.user.dto;
 
-import com.B108.tripwish.domain.user.entity.User;
-import lombok.*;
-
-import java.io.File;
 import java.time.LocalDate;
+
+import com.B108.tripwish.domain.user.entity.User;
+
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequestDto {
-    private String email;
-    private String password;
-    private String provider;
-    private String nickname;
-    private User.Gender gender;
-    private LocalDate birth;
-    private String phone;
-
+  private String email;
+  private String password;
+  private String provider;
+  private String nickname;
+  private User.Gender gender;
+  private LocalDate birth;
+  private String phone;
 }
