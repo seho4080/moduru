c426c845e404e9054290eda18f057d2760cbe2ee
SSAFY <chera1009@naver.com>
2025-07-31 11:11:28 +0900

    [FEATURE] RoomService enterRoom 구현

--- back/src/main/java/com/B108/tripwish/domain/room/service/RoomService.java
+++ back/src/main/java/com/B108/tripwish/domain/room/service/RoomService.java
@@ -1,7 +1,10 @@
 package com.B108.tripwish.domain.room.service;
 
 import com.B108.tripwish.domain.room.dto.response.TravelRoomCreateResponseDto;
+import com.B108.tripwish.domain.room.dto.response.TravelRoomResponseDto;
 
 public interface RoomService {
     TravelRoomCreateResponseDto addRoom();
+
+    TravelRoomResponseDto enterRoom(Long roomId);
 }
