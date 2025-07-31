0277d0f68593df90b92d979fc8040fd5015d179f
SSAFY <chera1009@naver.com>
2025-07-31 11:11:28 +0900

    [FEATURE] RoomService enterRoom 구현

--- back/src/main/java/com/B108/tripwish/domain/room/repository/TravelRoomRepository.java
+++ back/src/main/java/com/B108/tripwish/domain/room/repository/TravelRoomRepository.java
@@ -1,8 +1,13 @@
 package com.B108.tripwish.domain.room.repository;
 
+import com.B108.tripwish.domain.room.dto.response.TravelRoomResponseDto;
 import com.B108.tripwish.domain.room.entity.TravelRoom;
 import com.B108.tripwish.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TravelRoomRepository extends JpaRepository<TravelRoom, Long> {
+
+    Optional<TravelRoom> findByRoomId(Long roomId);
 }
--- back/src/main/java/com/B108/tripwish/domain/room/service/RoomServiceImpl.java
+++ back/src/main/java/com/B108/tripwish/domain/room/service/RoomServiceImpl.java
@@ -1,8 +1,11 @@
 package com.B108.tripwish.domain.room.service;
 
 import com.B108.tripwish.domain.room.dto.response.TravelRoomCreateResponseDto;
+import com.B108.tripwish.domain.room.dto.response.TravelRoomResponseDto;
 import com.B108.tripwish.domain.room.entity.TravelRoom;
 import com.B108.tripwish.domain.room.repository.TravelRoomRepository;
+import com.B108.tripwish.global.exception.CustomException;
+import com.B108.tripwish.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,8 +27,14 @@ public class RoomServiceImpl implements RoomService{
                 .title("이름 없는 사용자"+"_"+ LocalDateTime.now())
                 .build();
         travelRoomRepository.save(room);
-
         TravelRoomCreateResponseDto response = new TravelRoomCreateResponseDto(room.getRoomId());
+        return response;
+    }
+
+    @Override
+    public TravelRoomResponseDto enterRoom(Long roomId) {
+        TravelRoom room =  travelRoomRepository.findByRoomId(roomId).orElseThrow(() -> new CustomException(ErrorCode.ROOM_NOT_FOUND));
+        TravelRoomResponseDto response = TravelRoomResponseDto.from(room);
         return response;
     }
 }
