dd714bcced51590f354b261adc12ab15ebd16f2f
Leehyojin <lizzie0315@naver.com>
2025-08-01 21:29:33 +0900

    [CHORE]여행방 기본키명 수정

--- back/src/main/java/com/B108/tripwish/domain/room/service/RoomServiceImpl.java
+++ back/src/main/java/com/B108/tripwish/domain/room/service/RoomServiceImpl.java
@@ -51,17 +51,17 @@ public class RoomServiceImpl implements RoomService {
                 .travelRoom(room)
                 .user(user.getUser())
                 .role(TravelMemberRole.OWNER)
-                .id(new TravelMemberId(room.getRoomId(), user.getUser().getId()))
+                .id(new TravelMemberId(room.getId(), user.getUser().getId()))
                 .build();
         travelMemberRepository.save(member);
 
-        TravelRoomCreateResponseDto response = new TravelRoomCreateResponseDto(room.getRoomId());
+        TravelRoomCreateResponseDto response = new TravelRoomCreateResponseDto(room.getId());
         return response;
     }
 
     @Override
     public TravelRoomResponseDto enterRoom(CustomUserDetails user, Long roomId) {
-        TravelRoom room =  travelRoomRepository.findByRoomId(roomId).orElseThrow(() -> new CustomException(ErrorCode.ROOM_NOT_FOUND));
+        TravelRoom room =  travelRoomRepository.findById(roomId).orElseThrow(() -> new CustomException(ErrorCode.ROOM_NOT_FOUND));
         TravelRoomResponseDto response = TravelRoomResponseDto.from(room);
         return response;
     }
@@ -69,7 +69,7 @@ public class RoomServiceImpl implements RoomService {
     @Transactional
     @Override
     public TravelRoomResponseDto updateRoom(Long roomId, UpdateTravelRoomRequestDto request) {
-        TravelRoom room =  travelRoomRepository.findByRoomId(roomId).orElseThrow(() -> new CustomException(ErrorCode.ROOM_NOT_FOUND));
+        TravelRoom room =  travelRoomRepository.findById(roomId).orElseThrow(() -> new CustomException(ErrorCode.ROOM_NOT_FOUND));
         travelRoomMapper.updateFromDto(request, room);
         return travelRoomMapper.toDto(room);
     }
@@ -78,12 +78,12 @@ public class RoomServiceImpl implements RoomService {
     @Override
     public void deleteRoom(CustomUserDetails user, Long roomId) {
         Long userId = user.getUser().getId();
-        TravelMember member = travelMemberRepository.findByUser_IdAndTravelRoom_RoomId(userId, roomId)
+        TravelMember member = travelMemberRepository.findByUser_IdAndTravelRoom_Id(userId, roomId)
                 .orElseThrow(() -> new CustomException(ErrorCode.ROOM_MEMBER_NOT_FOUND));
         if (member.getRole() != TravelMemberRole.OWNER) {
             throw new CustomException(ErrorCode.ROOM_DELETE_FORBIDDEN); // 권한 없음
         }
 
-        travelRoomRepository.deleteByRoomId(roomId);
+        travelRoomRepository.deleteById(roomId);
     }
 }
