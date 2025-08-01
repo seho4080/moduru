cf2b7fa5775c2458828a25b8dc11734dd8669ccc
Leehyojin <lizzie0315@naver.com>
2025-08-01 09:32:57 +0900

    [CHORE]회원정보 수정 및 삭제 Operation 추가

--- back/src/main/java/com/B108/tripwish/domain/user/controller/UserController.java
+++ back/src/main/java/com/B108/tripwish/domain/user/controller/UserController.java
@@ -39,6 +39,20 @@ public class UserController {
     return ResponseEntity.ok(new CommonResponse("SIGNUP_SUCCESS", "회원가입이 완료되었습니다."));
   }
 
+
+  @Operation(
+          summary = "회원정보 수정",
+          description = "사용자가 수정할 정보를 입력하여 회원 정보를 수정합니다.",
+          requestBody =
+          @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                  description = "회원 정보 수정 요청 DTO",
+                  required = true),
+          responses = {
+                  @ApiResponse(responseCode = "200", description = "회원 정보 수정 성공"),
+                  @ApiResponse(responseCode = "400", description = "잘못된 요청 형식"),
+                  @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음"),
+                  @ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content)
+          })
   @PutMapping("/{userId}")
   public ResponseEntity<CommonResponse> updateUser(
       @PathVariable Long userId, @RequestBody UpdateUserRequestDto request) {
@@ -46,6 +60,16 @@ public class UserController {
     return ResponseEntity.ok(new CommonResponse("USER_UPDATED", "회원 정보가 수정되었습니다."));
   }
 
+
+  @Operation(
+          summary = "회원정보 삭제",
+          description = "사용자의 ID를 통해 회원 정보를 삭제합니다.",
+          responses = {
+                  @ApiResponse(responseCode = "200", description = "회원 정보 삭제 성공"),
+                  @ApiResponse(responseCode = "404", description = "삭제할 사용자를 찾을 수 없음"),
+                  @ApiResponse(responseCode = "400", description = "잘못된 요청 형식"),
+                  @ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content)
+          })
   @DeleteMapping("/{userId}")
   public ResponseEntity<CommonResponse> deleteUser(@PathVariable Long userId) {
     userService.deleteUser(userId);
