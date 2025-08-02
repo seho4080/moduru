0c8aa0dbef0a8b9b49492a3b1626fa9e5e6316ef
joyeun00 <joyeun00@naver.com>
2025-08-03 00:01:56 +0900

    [FEATURE]웹소켓 연결, 로그인 인증 수정

--- front/moduru/src/redux/slices/mapSlice.js
+++ front/moduru/src/redux/slices/mapSlice.js
@@ -1,11 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+// builtin
+
+// external
+import { createSlice } from "@reduxjs/toolkit";
+
+// internal
+
+// relative
 
 /**
  * 지도 관련 상태를 관리하는 Redux Slice
  * - 핀 목록 관리 (추가, 삭제)
  */
 const mapSlice = createSlice({
-  name: 'map',
+  name: "map",
 
   /** @type {{ pins: { id: string, lat: number, lng: number }[] }} */
   initialState: {
@@ -15,8 +22,8 @@ const mapSlice = createSlice({
   reducers: {
     /**
      * 핀 추가
-     * @param {Draft<State>} state - 현재 상태
-     * @param {PayloadAction<{ id: string, lat: number, lng: number }>} action - 추가할 핀 정보
+     * @param {import('immer').Draft<State>} state - 현재 상태
+     * @param {import('@reduxjs/toolkit').PayloadAction<{ id: string, lat: number, lng: number }>} action - 추가할 핀 정보
      */
     addPin: (state, action) => {
       state.pins.push(action.payload);
@@ -24,8 +31,8 @@ const mapSlice = createSlice({
 
     /**
      * 핀 삭제
-     * @param {Draft<State>} state - 현재 상태
-     * @param {PayloadAction<string>} action - 삭제할 핀의 ID
+     * @param {import('immer').Draft<State>} state - 현재 상태
+     * @param {import('@reduxjs/toolkit').PayloadAction<string>} action - 삭제할 핀의 ID
      */
     removePin: (state, action) => {
       state.pins = state.pins.filter((pin) => pin.id !== action.payload);
