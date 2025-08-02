2a254fe26047867f0db75f01c9bb5ff9e2c314a1
seho <tkrhktlqdjr1@naver.com>
2025-08-02 15:41:39 +0900

    [CHORE] back delete

--- back/src/main/java/com/B108/tripwish/domain/auth/dto/TokenType.java
+++
@@ -1,6 +0,0 @@
-package com.B108.tripwish.domain.auth.dto;
-
-public enum TokenType {
-  ACCESS,
-  REFRESH;
-}
--- back/src/main/java/com/B108/tripwish/domain/user/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.B108.tripwish.domain.user.service;
-
-import com.B108.tripwish.domain.user.dto.SignUpRequestDto;
-
-public interface UserService {
-  void addUser(SignUpRequestDto request);
-}
