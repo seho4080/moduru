c444d7df0397cccc390ef28299ab5331f03ab0b6
seho <tkrhktlqdjr1@naver.com>
2025-08-02 15:20:13 +0900

    test branch back conflict해결

--- back/src/main/java/com/B108/tripwish/domain/user/service/UserService.java
+++ back/src/main/java/com/B108/tripwish/domain/user/service/UserService.java
@@ -3,7 +3,5 @@ package com.B108.tripwish.domain.user.service;
 import com.B108.tripwish.domain.user.dto.SignUpRequestDto;
 
 public interface UserService {
-    void addUser(SignUpRequestDto request);
-
-
+  void addUser(SignUpRequestDto request);
 }
