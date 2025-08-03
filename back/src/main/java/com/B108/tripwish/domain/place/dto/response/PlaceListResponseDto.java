b76418689c8b5f45e0983f32548799fef97efa0c
SSAFY <chera1009@naver.com>
2025-08-04 08:45:25 +0900

    [FEAT] ElasticSearch 기반 장소 목록 조회 구현

--- back/src/main/java/com/B108/tripwish/domain/place/dto/response/PlaceListResponseDto.java
+++ back/src/main/java/com/B108/tripwish/domain/place/dto/response/PlaceListResponseDto.java
@@ -5,11 +5,10 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlaceListResponseDto {
-  private List<PlaceDto> places;
+  private List<PlaceResponseDto> places;
 }
