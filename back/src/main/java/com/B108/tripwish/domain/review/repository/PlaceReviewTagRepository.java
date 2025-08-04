90d7d6eb2b811fc47d505087900f10ee4b0eb3b9
이체라 <chera1009@naver.com>
2025-08-04 12:12:11 +0900

    fix: 오류수정

---
+++ back/src/main/java/com/B108/tripwish/domain/review/repository/PlaceReviewTagRepository.java
@@ -0,0 +1,17 @@
+package com.B108.tripwish.domain.review.repository;
+
+import com.B108.tripwish.domain.review.entity.PlaceReviewTag;
+import com.B108.tripwish.domain.review.entity.PlaceReviewTagId;
+import com.B108.tripwish.domain.review.entity.ReviewTag;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PlaceReviewTagRepository extends JpaRepository<PlaceReviewTag, PlaceReviewTagId> {
+
+    @Query("SELECT pt.tag.content FROM PlaceReviewTag pt WHERE pt.review.place.id = :placeId")
+    List<String> findTagNamesByPlaceId(@Param("placeId") Long placeId);
+
+}
