99131e1f7076fd90fa31fb8cb9710c6576db32d3
Leehyojin <lizzie0315@naver.com>
2025-08-04 10:23:53 +0900

    [FEAT]유저가 해당하는 여행방 목록 조회

--- back/src/main/java/com/B108/tripwish/domain/room/repository/TravelMemberRepository.java
+++ back/src/main/java/com/B108/tripwish/domain/room/repository/TravelMemberRepository.java
@@ -1,5 +1,6 @@
 package com.B108.tripwish.domain.room.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,7 @@ import com.B108.tripwish.domain.room.entity.TravelMember;
 
 public interface TravelMemberRepository extends JpaRepository<TravelMember, Long> {
   Optional<TravelMember> findByUser_IdAndTravelRoom_RoomId(Long userId, Long travelRoomId);
+
+  //유저가 속한 여행 방 조회용
+  List<TravelMember> findByUser_Id(Long userId);
 }
