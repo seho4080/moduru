2791802542bd994e74f29eca70525e6cbc29458c
SSAFY <chera1009@naver.com>
2025-08-04 11:12:45 +0900

    [FIX] 오류 수정

--- back/src/main/java/com/B108/tripwish/domain/review/repository/ReviewRepository.java
+++ back/src/main/java/com/B108/tripwish/domain/review/repository/ReviewRepository.java
@@ -3,7 +3,9 @@ package com.B108.tripwish.domain.review.repository;
 import com.B108.tripwish.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+import java.util.List;
 
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<String> findTagNamesByPlaceId(Long placeId);
 
 }
