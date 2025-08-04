fb44da9bd2e3b3eb551edbf3ddae0030546c47ed
Leehyojin <lizzie0315@naver.com>
2025-08-04 10:23:53 +0900

    [FEAT]유저가 해당하는 여행방 목록 조회

--- back/src/main/java/com/B108/tripwish/domain/user/service/UserService.java
+++ back/src/main/java/com/B108/tripwish/domain/user/service/UserService.java
@@ -4,6 +4,9 @@ import com.B108.tripwish.domain.auth.service.CustomUserDetails;
 import com.B108.tripwish.domain.user.dto.request.SignUpRequestDto;
 import com.B108.tripwish.domain.user.dto.request.UpdateUserRequestDto;
 import com.B108.tripwish.domain.user.dto.response.InfoUserResponseDto;
+import com.B108.tripwish.domain.user.dto.response.UserTravelRoomResponseDto;
+
+import java.util.List;
 
 public interface UserService {
   void addUser(SignUpRequestDto request);
@@ -13,4 +16,6 @@ public interface UserService {
   void updateUser(CustomUserDetails currentUser, UpdateUserRequestDto request);
 
   void deleteUser(CustomUserDetails currentUser);
+
+  List<UserTravelRoomResponseDto> getUserTravelRooms(CustomUserDetails currentUser);
 }
