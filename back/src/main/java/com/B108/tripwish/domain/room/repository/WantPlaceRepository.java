70ec6b55f68473359873ccbce49015ec61c1ff86
SSAFY <chera1009@naver.com>
2025-08-05 17:58:49 +0900

    [FEAT] 장소 투표 DB 저장 기능 구현

--- back/src/main/java/com/B108/tripwish/domain/room/repository/WantPlaceRepository.java
+++ back/src/main/java/com/B108/tripwish/domain/room/repository/WantPlaceRepository.java
@@ -3,6 +3,12 @@ package com.B108.tripwish.domain.room.repository;
 import com.B108.tripwish.domain.room.entity.WantPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WantPlaceRepository extends JpaRepository<WantPlace, Long> {
     boolean existsByTravelRoom_IdAndPlace_Id(Long roomId, Long placeId);
+
+    Optional<WantPlace> findByPlace_Id(Long placeId);
+
+
 }
--- back/src/main/java/com/B108/tripwish/domain/room/service/WantPlaceReaderService.java
+++ back/src/main/java/com/B108/tripwish/domain/room/service/WantPlaceReaderService.java
@@ -1,6 +1,13 @@
 package com.B108.tripwish.domain.room.service;
 
+import com.B108.tripwish.domain.room.entity.WantPlace;
+
 public interface WantPlaceReaderService {
 
     boolean isWanted(Long roomId, Long placeId);
+
+    boolean isVotedByUser(Long userId, Long wantPlaceId);
+
+    long getVoteCount(Long wantPlaceId, WantPlace wantPlace);
+
 }
