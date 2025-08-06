6e4c7931b5aa30dbfe37571e29efb527a1639353
joyeun00 <joyeun00@naver.com>
2025-08-06 21:14:39 +0900

    [FEAT] 웹소켓 장소 공유 구현
    Former-commit-id: 0af19482d1dd92b0a76b3b85134b9a195b7962c2

--- front/moduru/src/redux/store.js
+++ front/moduru/src/redux/store.js
@@ -1,16 +1,18 @@
 // src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
-import mapReducer from "./slices/mapSlice"; // 핀 목록 관리
+import pinReducer from "./slices/pinSlice"; // 핀 목록 관리
 import likedPlaceReducer from "./slices/likedPlaceSlice";
 import wishPlaceReducer from "./slices/wishPlaceSlice";
+import mapReducer from "./slices/mapSlice";
 /**
  * Redux 전역 상태 저장소 설정
  */
 const store = configureStore({
   reducer: {
-    map: mapReducer,
+    pin: pinReducer,
     likedPlace: likedPlaceReducer,
     wishPlace: wishPlaceReducer,
+    map: mapReducer,
   },
 });
 
