1de85a0502a4bf647cca9a2cb9454c23f64aeb87
하다예 <abcdaye@naver.com>
2025-08-06 21:38:18 +0900

    [FEAT]링크를 통한 초대
    Former-commit-id: 3a96f39914d9b1f2a76804b78c79a6c026d010f9

--- front/moduru/src/redux/store.js
+++ front/moduru/src/redux/store.js
@@ -3,6 +3,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import mapReducer from "./slices/mapSlice"; // 핀 목록 관리
 import likedPlaceReducer from "./slices/likedPlaceSlice";
 import wishPlaceReducer from "./slices/wishPlaceSlice";
+import tripMemberReducer from './slices/tripMemberSlice';
+import friendReducer from './slices/friendSlice';
+import tripRoomReducer from './slices/tripRoomSlice';
 /**
  * Redux 전역 상태 저장소 설정
  */
@@ -11,6 +14,9 @@ const store = configureStore({
     map: mapReducer,
     likedPlace: likedPlaceReducer,
     wishPlace: wishPlaceReducer,
+    tripMember: tripMemberReducer,
+    friend: friendReducer,
+    tripRoom: tripRoomReducer,
   },
 });
 
