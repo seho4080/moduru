ee66c755ae37aadf0f89bf4f5ec65bc6ff4bf2d7
SSAFY <chera1009@naver.com>
2025-08-07 09:57:28 +0900

    [FIX] 백에드 오류 수정
    Former-commit-id: 29aa0a9b4af97840189c3a62cfa9f659b2855786

--- back/src/main/java/com/B108/tripwish/websocket/service/PlaceWantSocketService.java
+++ back/src/main/java/com/B108/tripwish/websocket/service/PlaceWantSocketService.java
@@ -30,13 +30,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PlaceWantSocketService {
 
-  private final RedisPublisher redisPublisher;
-  private final SimpMessageSendingOperations messagingTemplate;
-  private final RoomReaderService roomReaderService;
-  private final WantPlaceReaderService wantPlaceReaderService;
-  private final WantPlaceService wantPlaceService;
-  private final MyPlaceReaderService myPlaceReaderService;
-  private final PlaceReaderService placeReaderService;
+    private final RedisPublisher redisPublisher;
+    private final SimpMessageSendingOperations messagingTemplate;
+    private final RoomReaderService roomReaderService;
+    private final WantPlaceReaderService wantPlaceReaderService;
+    private final WantPlaceService wantPlaceService;
+    private final MyPlaceReaderService myPlaceReaderService;
+    private final PlaceReaderService placeReaderService;
 
     public void handleAdd(CustomUserDetails sender, Long roomId, PlaceWantAddRequestDto request) {
         Long refId = request.getId();
@@ -103,7 +103,7 @@ public class PlaceWantSocketService {
         messagingTemplate.convertAndSend("/topic/room/" + roomId + "/place-want/add", response);
     }
 
-    public void handleRemove(CustomUserDetails sender, Long roomId, PlaceWantRemoveRequestDto request){
+    public void handleRemove(CustomUserDetails sender, Long roomId, PlaceWantRemoveRequestDto request) {
 
         // 희망장소 삭제
         wantPlaceService.removeWantPlace(roomId, request.getWantId());
@@ -120,75 +120,4 @@ public class PlaceWantSocketService {
         }
         messagingTemplate.convertAndSend("/topic/room/" + roomId + "/place-want/remove", response);
     }
-
-    WantPlace saved = wantPlaceService.saveWantPlace(roomId, type, refId);
-    Long wantId = saved.getId();
-
-    // 장소 기본 정보 세팅
-    Double lat;
-    Double lng;
-    String imgUrl;
-    String placeName;
-    String address;
-    String category;
-
-    if (type == PlaceType.CUSTOM) {
-      CustomPlace customPlace = wantPlaceReaderService.getCustomPlaceById(refId);
-      lat = customPlace.getLat();
-      lng = customPlace.getLng();
-      imgUrl = null;
-      category = null;
-      placeName = customPlace.getName();
-      address = customPlace.getAddress();
-    } else if (type == PlaceType.PLACE) {
-      Place place = placeReaderService.findPlaceById(refId);
-      place.getImages().size();
-      lat = place.getLat();
-      lng = place.getLng();
-      category = place.getCategory().getCategoryName();
-      List<PlaceImage> images = place.getImages();
-      imgUrl = (images != null && !images.isEmpty()) ? images.get(0).getImgUrl() : null;
-      placeName = place.getPlaceName();
-      address = place.getRoadAddressName();
-    } else {
-      throw new CustomException(ErrorCode.UNSUPPORTED_PLACE_TYPE);
-    }
-    PlaceWantAddMessageResponseDto response =
-        PlaceWantAddMessageResponseDto.builder()
-            .type(request.getType())
-            .id(request.getId())
-            .roomId(request.getRoomId())
-            .wantId(wantId)
-            .sendId(sender.getUser().getUuid().toString())
-            .category(category)
-            .placeName(placeName)
-            .address(address)
-            .lat(lat)
-            .lng(lng)
-            .imgUrl(imgUrl)
-            .voteCnt(0L)
-            .isVoted(false)
-            .build();
-
-    messagingTemplate.convertAndSend("/topic/room/" + roomId + "/place-want/add", response);
-  }
-
-  public void handleRemove(
-      CustomUserDetails sender, Long roomId, PlaceWantRemoveRequestDto request) {
-
-    // 희망장소 삭제
-    wantPlaceService.removeWantPlace(roomId, request.getWantId());
-
-    // 메시지 생성
-    PlaceWantRemoveMessageResponseDto response =
-        new PlaceWantRemoveMessageResponseDto(
-            request.getWantId(), request.getRoomId(), sender.getUser().getUuid().toString());
-
-    // 방에 속한 모든 유저에게 전송
-    List<User> usersInRoom = roomReaderService.findUsersByRoomId(roomId);
-    for (User u : usersInRoom) {
-      messagingTemplate.convertAndSendToUser(u.getUuid().toString(), "/queue/place-want", response);
-    }
-    messagingTemplate.convertAndSend("/topic/room/" + roomId + "/place-want/remove", response);
-  }
 }
