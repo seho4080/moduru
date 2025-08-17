d3718395579c2aa443163e8f4f665bb65c90b8a6
DESKTOP-RBNO6LS\SSAFY <chera1009@naver.com>
2025-08-17 21:56:36 +0900

    [FEAT] 장소 투표 기능 추가
    Former-commit-id: 1f5320d64db966a44b5aa87b48a0fe000ca4c859

--- front/moduru/src/features/placeVote/model/usePlaceVoteSocket.js
+++ front/moduru/src/features/placeVote/model/usePlaceVoteSocket.js
@@ -2,51 +2,47 @@
 import { useEffect, useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { connectWebSocket, unsubscribeKeys } from "../../webSocket/coreSocket";
-import { upsertVoteState } from "../../../redux/slices/placeVoteSlice";
+import { updateVoteState } from "../../../redux/slices/sharedPlaceSlice";
 
 export default function usePlaceVoteSocket(roomId) {
   const dispatch = useDispatch();
+
+  console.log("[usePlaceVoteSocket] 호출됨, roomId:", roomId);
 
   const subKeys = useMemo(() => {
     if (!roomId) return [];
     return [
       `${roomId}|/topic/room/${roomId}/place-vote`,
-      `${roomId}|/user/queue/place-vote`,
     ];
   }, [roomId]);
 
   useEffect(() => {
-    if (!roomId) return;
+    if (!roomId) {
+      console.log("[usePlaceVoteSocket] roomId 없음, 구독 생략");
+      return;
+    }
 
+    console.log("[usePlaceVoteSocket] WebSocket 구독 시작, roomId:", roomId);
     connectWebSocket(roomId, [
-      // 1) 방 전체 브로드캐스트
+      // 1) 방 전체 브로드캐스트 (voteCnt만)
       {
-        dest: `/topic/room/${roomId}/place-vote`,
+        handler: "place-vote",
         callback: (msg) => {
           // { wantId or wantPlaceId, voteCnt, receiverId? }
           const wantId = msg.wantId ?? msg.wantPlaceId;
           const { voteCnt } = msg;
           if (wantId == null) return;
-          dispatch(upsertVoteState({ wantId, voteCnt }));
-        },
-      },
-      // 2) 개인 큐 (서버 convertAndSendToUser)
-      {
-        dest: `/user/queue/place-vote`,
-        // 또는 personal: "place-vote" 만 넘겨도 됨
-        // personal: "place-vote",
-        callback: (msg) => {
-          // { roomId, wantId, isVoted, receiverId }
-          const wantId = msg.wantId ?? msg.wantPlaceId;
-          const { isVoted } = msg;
-          if (wantId == null) return;
-          dispatch(upsertVoteState({ wantId, isVoted }));
+          console.log("[WebSocket] 브로드캐스트 수신:", { wantId, voteCnt });
+          dispatch(updateVoteState({ wantId, voteCnt }));
         },
       },
     ]);
 
     return () => {
-      if (subKeys.length) unsubscribeKeys?.(subKeys);
+      if (subKeys.length) {
+        console.log("[usePlaceVoteSocket] 구독 해제:", subKeys);
+        unsubscribeKeys?.(subKeys);
+      }
     };
   }, [roomId, subKeys, dispatch]);
 }
